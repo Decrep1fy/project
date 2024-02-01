74fee6fb5971753e9c24d0cf80978a156ebb0027
Oleg <vosknik@gmail.com>
2024-02-01 22:39:04 +0200

    Apple Quest

--- js/script.js
+++ js/script.js
@@ -1,47 +1,18 @@
 "use strict";
-if (3) {
-    console.log ('Ok!');
-} else {
-    console.log(`error`);
-}
 
-// const num = 50;
+const countOfApple = +prompt(`Cколько яблок у тебя есть?`,``);
+const countOfGive = +prompt (`Cколько хочешь отдать?`,``);
 
-// if (num < 49) {
-//     console.log('error');
-// } else if (num > 100) {
-//     console.log(`дохуя`);
-// }else {
-//     console.log(`oke!`);
+if (countOfApple < countOfGive){
+    console.log(`иди нахуй!`);
+} else console.log (countOfApple - countOfGive)
+
+// const appleCount = prompt("Сколько у вас яблок?"); // Введите количество яблок
+// const toGiveAway = prompt("Сколько яблок вы хотите отдать?"); // Введите количество яблок, которые хотите отдать
+
+// if (toGiveAway > appleCount) {
+//   console.log("Нельзя отдать больше яблок, чем у вас есть!");
+// } else {
+//   const remainingApples = appleCount - toGiveAway;
+//   console.log(`У вас осталось ${remainingApples} яблок.`);
 // }
-
-// (num === 50) ? console.log(`oke!`) : console.log(`error`);
-
-const accountName = "Mil1furion";
-
-switch (accountName) {
-    default:
-        console.log(`иди нахуй!`);
-        break;
-    case "Milfurion":
-        console.log(`Да!`);
-        break;
-}
-
-const accountPass = "Redfox1strategi";
-
-switch (accountPass) {
-    default:
-        console.log(`иди нахуй!`);
-        break;
-    case "Redfoxsstrategi":
-        console.log(`Да!`);
-        break;
-}
-
-if (accountName === 'Milfurion'&& accountPass === `Redfoxsstrategi`){
-    console.log(`Да!`);
-} else{
-    console.log(`Пошел нахуй!`);
-}
-
