2f34cace9a43e59e7b6a964a3e7528ecf9d73158
Oleg <vosknik@gmail.com>
2024-02-01 22:07:49 +0200

    AccauntName

--- js/script.js
+++ js/script.js
@@ -1,21 +1,47 @@
 "use strict";
-const numberOfFilms = + prompt(`Сколько фильмов вы уже посмотрели`, ``);
+if (3) {
+    console.log ('Ok!');
+} else {
+    console.log(`error`);
+}
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: [],
-    actirs: {},
-    geners: [],
-    privat: false
-};
+// const num = 50;
 
-const a = prompt(`Один из последних фильмов?`, ``),
-      b = prompt('На сколько оцените его?',``),
-      c = prompt('один из последних просмотреных фильмов?',``),
-      d = prompt(`На сколько вы его оцените`, ``);
-  
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-   
-console.log(personalMovieDB);
-  
+// if (num < 49) {
+//     console.log('error');
+// } else if (num > 100) {
+//     console.log(`дохуя`);
+// }else {
+//     console.log(`oke!`);
+// }
+
+// (num === 50) ? console.log(`oke!`) : console.log(`error`);
+
+const accountName = "Mil1furion";
+
+switch (accountName) {
+    default:
+        console.log(`иди нахуй!`);
+        break;
+    case "Milfurion":
+        console.log(`Да!`);
+        break;
+}
+
+const accountPass = "Redfox1strategi";
+
+switch (accountPass) {
+    default:
+        console.log(`иди нахуй!`);
+        break;
+    case "Redfoxsstrategi":
+        console.log(`Да!`);
+        break;
+}
+
+if (accountName === 'Milfurion'&& accountPass === `Redfoxsstrategi`){
+    console.log(`Да!`);
+} else{
+    console.log(`Пошел нахуй!`);
+}
+
