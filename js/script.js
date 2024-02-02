954d553000f1eeb88461eceb1cfd6c8274f7ac2b
Oleg <vosknik@gmail.com>
2024-02-02 19:11:20 +0200

    MovieDBmodcount

--- js/script.js
+++ js/script.js
@@ -1,18 +1,53 @@
-"use strict";
+/* Задание на урок:
 
-const countOfApple = +prompt(`Cколько яблок у тебя есть?`,``);
-const countOfGive = +prompt (`Cколько хочешь отдать?`,``);
+1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
 
-if (countOfApple < countOfGive){
-    console.log(`иди нахуй!`);
-} else console.log (countOfApple - countOfGive)
+2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
+возвращаем пользователя к вопросам опять
 
-// const appleCount = prompt("Сколько у вас яблок?"); // Введите количество яблок
-// const toGiveAway = prompt("Сколько яблок вы хотите отдать?"); // Введите количество яблок, которые хотите отдать
+3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
+"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
 
-// if (toGiveAway > appleCount) {
-//   console.log("Нельзя отдать больше яблок, чем у вас есть!");
-// } else {
-//   const remainingApples = appleCount - toGiveAway;
-//   console.log(`У вас осталось ${remainingApples} яблок.`);
-// }
+4) Потренироваться и переписать цикл еще двумя способами*/
+
+'use strict';
+
+// Код возьмите из предыдущего домашнего задания
+
+const numberOfFilms = +prompt(`Сколько фильмов вы уже посмотрели`, ``);
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actirs: {},
+    geners: [],
+    privat: false
+};
+
+for(let i = 0; i < 2; i++) {
+    const a = prompt(`Один из последних фильмов?`, ``),
+          b = prompt('На сколько оцените его?',``);
+    
+if (a != null && b != null && a != `` && b != `` && a.length < 50){
+    personalMovieDB.movies[a] = b;
+    console.log('done!');
+} else {
+    console.log(`error!`);
+    i--;
+}
+
+        personalMovieDB.movies[a] = b;
+        }
+        console.log(personalMovieDB);
+
+        if (personalMovieDB.count < 10){
+            console.log(`Просмотрено довольно мало фильмов`);
+            } else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+                console.log (`Вы классический зритель`);
+            } else if (personalMovieDB.count >=30){
+                console.log(`Вы киноман!`);
+            }else{
+                console.log(`Произошла ошибка!`);
+            }
